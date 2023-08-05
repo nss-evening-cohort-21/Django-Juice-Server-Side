d778f3ab49acc0b0939bf3eee27feeade1dffd2b
DeCorrean Davis <112592380+DeCorreanD@users.noreply.github.com>
2023-08-05 09:59:25 -0500

    Made changes to the decimal

--- djangojuiceapi/models/item.py
+++ djangojuiceapi/models/item.py
@@ -3,7 +3,7 @@ from django.db import models
 class Item(models.Model):
   
     name = models.CharField(max_length=50)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
     image_url = models.URLField()
     description = models.CharField(max_length=100)
     
